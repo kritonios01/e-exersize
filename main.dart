6d0789c8b8a5da56f5947268f90e107e8b9eaaac
achilleastheo <achilleas.theocharopoulos@gmail.com>
2024-01-19 18:28:32 +0100

    Update with second page

--- main.dart
+++ main.dart
@@ -8,59 +8,96 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              // First block
-              Container(
-                height: (MediaQuery.of(context).size.height - 400) / 4,
-                color: Colors.red[900], // Use a deep red color
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Text(
-                        'Connecting Athletes',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            // First block
+            Expanded(
+              child: DefaultTextStyle(
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Theme.of(context).textTheme.bodyText1!.fontFamily,
+                ),
+                child: Container(
+                  color: Color(0xFF800000), // Burgundy color
+                  child: Stack(
+                    children: [
+                      Center(
+                        child: Text('Connecting Athletes'),
+                      ),
+                      Positioned(
+                        top: 16.0,
+                        right: 16.0,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.info,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            // Add your info icon tap logic here
+                          },
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 16.0,
-                      right: 16.0,
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.info,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          // Add your info icon tap logic here
-                        },
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-              // Second block
-              Container(
-                height: 400.0,
-                color: Colors.white,
-                child: Image.asset(
-                  'assets/main_page_bball.png', // Replace with your actual image file name
-                  fit: BoxFit.cover,
-                ),
+            ),
+            // Second block
+            Container(
+              height: 400.0,
+              color: Colors.white,
+              child: Image.asset(
+                'assets/main_page_bball.png', // Replace with your actual image file name
+                fit: BoxFit.cover,
               ),
-              // Third block
-              Container(
-                height: (MediaQuery.of(context).size.height - 400) / 4,
-                color: Colors.red[900], // Use a deep red color
+            ),
+            // Third block
+            Expanded(
+              child: Container(
+                color: Color(0xFF800000), // Burgundy color
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
+                    Container(
+                      height: 50.0, // Adjust the height as needed
+                      color: Color(0xFF800000), // Burgundy color
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SearchTeamsPage(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFF6A1B9A), // Slightly darker purple
+                          ),
+                          child: Text(
+                            'Search for teams!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                     Container(
                       height: 50.0, // Adjust the height as needed
                       color: Colors.white,
@@ -107,25 +144,11 @@ class MyApp extends StatelessWidget {
                         ],
                       ),
                     ),
-                    Container(
-                      height: (MediaQuery.of(context).size.height - 400) / 4 - 50.0, // Adjust the height as needed
-                      color: Colors.purple, // Use your desired color here
-                      child: Center(
-                        child: Text(
-                          'Search for teams!',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -133,3 +156,274 @@ class MyApp extends StatelessWidget {
 }
 
 
+
+class SearchTeamsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            // First block
+            Expanded(
+              child: Container(
+                color: Color(0xFF800000), // Burgundy color
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        'Find your teams',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            // Second block
+            Container(
+              height: 400.0,
+              color: Colors.white,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    'assets/main_page_bball.png', // Replace with your actual image file name
+                    fit: BoxFit.cover,
+                    color: Colors.white.withOpacity(0.5), // Apply a white "fog" filter
+                    colorBlendMode: BlendMode.lighten,
+                  ),
+                  // Third block - Four purplish white background text frames
+                  Positioned(
+                    top: 50.0, // Adjust the top position based on your design
+                    left: 20.0, // Adjust the left position based on your design
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200], // Grayish white color
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      width: MediaQuery.of(context).size.width - 40.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Sport',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'What sport are you interested in',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 150.0, // Adjust the top position based on your design
+                    left: 20.0, // Adjust the left position based on your design
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200], // Grayish white color
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      width: MediaQuery.of(context).size.width - 40.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Region',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'Where do you want to meet',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 250.0, // Adjust the top position based on your design
+                    left: 20.0, // Adjust the left position based on your design
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200], // Grayish white color
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      width: MediaQuery.of(context).size.width - 40.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Time',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'When are you available',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 350.0, // Adjust the top position based on your design
+                    left: 20.0, // Adjust the left position based on your design
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200], // Grayish white color
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      width: MediaQuery.of(context).size.width - 40.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Duration',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'For how long',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Fourth block - Menu bar and Search for Teams button
+            Expanded(
+              child: Container(
+                color: Color(0xFF800000), // Burgundy color
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 50.0, // Adjust the height as needed
+                      color: Color(0xFF800000), // Burgundy color
+                      child: Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Add your "Search for teams!" button logic here
+                          },
+                          style: ElevatedButton.styleFrom(
+                            primary: Color(0xFF6A1B9A), // Slightly darker purple
+                          ),
+                          child: Text(
+                            'Find available teams',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50.0, // Adjust the height as needed
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.create),
+                            onPressed: () {
+                              // Add your create team logic here
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.home),
+                            onPressed: () {
+                              // Add your home logic here
+                            },
+                          ),
+                          Stack(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.notifications),
+                                onPressed: () {
+                                  // Add your notifications logic here
+                                },
+                              ),
+                              Positioned(
+                                top: 5.0,
+                                right: 5.0,
+                                child: CircleAvatar(
+                                  radius: 8.0,
+                                  backgroundColor: Colors.red,
+                                  child: Text(
+                                    '3',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
